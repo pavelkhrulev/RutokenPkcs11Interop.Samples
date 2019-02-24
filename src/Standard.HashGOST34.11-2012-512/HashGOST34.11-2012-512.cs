9ff63ed262cab6528d3afa35be41367777544c1c
Pavel Khrulev <pahan4igg@gmail.com>
2019-02-24 22:59:44 +0300

    #5 HashGost34.11-2012 256 and 512 samples

---
+++ src/Standard.HashGOST34.11-2012-512/HashGOST34.11-2012-512.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using Net.Pkcs11Interop.Common;
+using Net.Pkcs11Interop.HighLevelAPI;
+using RutokenPkcs11Interop;
+using RutokenPkcs11Interop.Common;
+using RutokenPkcs11Interop.Samples.Common;
+
+namespace Standard.HashGOST3411_2012_512
+{
+    /*************************************************************************
+    * Rutoken                                                                *
+    * Copyright (c) 2003-2019, CJSC Aktiv-Soft. All rights reserved.         *
+    * Подробная информация:  http://www.rutoken.ru                           *
+    *------------------------------------------------------------------------*
+    * Пример работы с Рутокен при помощи библиотеки PKCS#11 на языке C#      *
+    *------------------------------------------------------------------------*
+    * Использование команд вычисления хэш-кода:                              *
+    *  - установление соединения с Рутокен в первом доступном слоте;         *
+    *  - определение типа подключенного токена;                              *
+    *  - вычисление хэш-кода ГОСТ Р 34.11-2012(512);                         *
+    *  - закрытие соединения с Рутокен.                                      *
+    *------------------------------------------------------------------------*
+    * Данный пример является самодостаточным.                                *
+    *************************************************************************/
+
+    class HashGOST3411_2012_512
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                // Инициализировать библиотеку
+                Console.WriteLine("Library initialization");
+                using (var pkcs11 = new Pkcs11(Settings.RutokenEcpDllDefaultPath, Settings.OsLockingDefault))
+                {
+                    // Получить доступный слот
+                    Console.WriteLine("Checking tokens available");
+                    Slot slot = Helpers.GetUsableSlot(pkcs11);
+
+                    // Определение поддерживаемых токеном механизмов
+                    Console.WriteLine("Checking mechanisms available");
+                    List<CKM> mechanisms = slot.GetMechanismList();
+                    Errors.Check(" No mechanisms available", mechanisms.Count > 0);
+                    bool isGostR3411_12_512Supported = mechanisms.Contains((CKM)Extended_CKM.CKM_GOSTR3411_12_512);
+                    Errors.Check(" CKM_GOSTR3411_12_512 isn`t supported!", isGostR3411_12_512Supported);
+
+                    // Открыть RW сессию в первом доступном слоте
+                    Console.WriteLine("Opening RW session");
+                    using (Session session = slot.OpenSession(false))
+                    {
+                        // Выполнить аутентификацию Пользователя
+                        Console.WriteLine("User authentication");
+                        session.Login(CKU.CKU_USER, SampleConstants.NormalUserPin);
+
+                        try
+                        {
+                            // Получить данные для хэширования
+                            byte[] sourceData = SampleData.Digest_Gost3411_SourceData;
+
+                            // Инициализировать операцию хэширования
+                            var mechanism = new Mechanism((uint)Extended_CKM.CKM_GOSTR3411_12_512);
+
+                            // Вычислить хэш-код данных
+                            Console.WriteLine("Hashing data...");
+                            byte[] hash = session.Digest(mechanism, sourceData);
+
+                            // Распечатать буфер, содержащий хэш-код
+                            Console.WriteLine(" Hashed buffer is:");
+                            Helpers.PrintByteArray(hash);
+                            Console.WriteLine("Hashing has been completed successfully");
+                        }
+                        finally
+                        {
+                            // Сбросить права доступа как в случае исключения,
+                            // так и в случае успеха.
+                            // Сессия закрывается автоматически.
+                            session.Logout();
+                        }
+                    }
+                }
+            }
+            catch (Pkcs11Exception ex)
+            {
+                Console.WriteLine($"Operation failed [Method: {ex.Method}, RV: {ex.RV}]");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Operation failed [Message: {ex.Message}]");
+            }
+        }
+    }
+}
