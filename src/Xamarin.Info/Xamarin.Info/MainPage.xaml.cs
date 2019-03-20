6d71c178ef2fbfb4a24351e2b3d1acbb35ed05df
Pavel Khrulev <pahan4igg@gmail.com>
2019-03-21 01:56:05 +0300

    #1 User-friendly xaml

--- src/Xamarin.Info/Xamarin.Info/MainPage.xaml.cs
+++ src/Xamarin.Info/Xamarin.Info/MainPage.xaml.cs
@@ -1,13 +1,27 @@
 ﻿using Net.Pkcs11Interop.Common;
 using Net.Pkcs11Interop.HighLevelAPI;
 using RutokenPkcs11Interop;
-using RutokenPkcs11Interop.Common;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using RutokenPkcs11Interop.Common;
 using Xamarin.Forms;
 
 namespace Xamarin.Info
 {
+    public class Feature
+    {
+        public Feature(string name, string description)
+        {
+            Name = name;
+            Description = description;
+        }
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+
+    // TODO: Go to MVVM
     public partial class MainPage : ContentPage
     {
         public MainPage()
@@ -15,26 +29,34 @@ namespace Xamarin.Info
             InitializeComponent();
         }
 
+        private ObservableCollection<Feature> features;
+        public ObservableCollection<Feature> Features
+        {
+            get => features;
+            set
+            {
+                features = value;
+                OnPropertyChanged(nameof(Features));
+            }
+        }
+
         private void Button_OnClicked(object sender, EventArgs e)
         {
             try
             {
+                Features = new ObservableCollection<Feature>();
+
                 // Инициализировать библиотеку
-                InfoLabel.Text += ("Library initialization");
                 using (var pkcs11 = new Pkcs11(Settings.RutokenEcpDllDefaultPath, AppType.MultiThreaded))
                 {
                     // Получить информацию о библиотеке
-                    InfoLabel.Text = "Getting library info";
-                    LibraryInfo libraryInfo = pkcs11.GetInfo();
-                    InfoLabel.Text += ("Printing library info:");
-                    InfoLabel.Text += (" PKCS#11 version: {0}", libraryInfo.CryptokiVersion);
-                    InfoLabel.Text += (" Manufacturer: {0}", libraryInfo.ManufacturerId);
-                    InfoLabel.Text += (" Flags: 0x{0:X}", libraryInfo.Flags);
-                    InfoLabel.Text += (" Library description: {0}", libraryInfo.LibraryDescription);
-                    InfoLabel.Text += "\n";
+                    var libraryInfo = pkcs11.GetInfo();
+
+                    Features.Add(new Feature("PKCS#11 version", libraryInfo.CryptokiVersion));
+                    Features.Add(new Feature("Library manufacturer", libraryInfo.ManufacturerId));
+                    Features.Add(new Feature("Library description", libraryInfo.LibraryDescription));
 
                     // Получить слоты
-                    InfoLabel.Text += ("Checking slots available");
                     List<Slot> slots = pkcs11.GetSlotList(SlotsType.WithTokenPresent);
                     // Проверить, что слоты найдены
                     if (slots == null)
@@ -49,47 +71,29 @@ namespace Xamarin.Info
                     //        - о поддерживаемых механизмах.
                     foreach (var slot in slots)
                     {
-                        SlotInfo slotInfo = slot.GetSlotInfo();
+                        var slotInfo = slot.GetSlotInfo();
 
                         // Распечатать информацию о слоте
-                        InfoLabel.Text += ("Printing slot info:");
-                        InfoLabel.Text += (" Slot description: {0}", slotInfo.SlotDescription);
-                        InfoLabel.Text += (" Manufacturer: {0}", slotInfo.ManufacturerId);
-                        InfoLabel.Text += (" Flags: 0x{0:X}", slotInfo.SlotFlags.Flags);
-                        InfoLabel.Text += (" Hardware version: {0}", slotInfo.HardwareVersion);
-                        InfoLabel.Text += (" Firmware version: {0}", slotInfo.FirmwareVersion);
-                        InfoLabel.Text += "\n";
+                        Features.Add(new Feature("Slot description", slotInfo.SlotDescription));
+                        Features.Add(new Feature("Slot manufacturer", slotInfo.ManufacturerId));
+                        Features.Add(new Feature("Slot hardware version", slotInfo.HardwareVersion));
+                        Features.Add(new Feature("Slot firmware version", slotInfo.FirmwareVersion));
 
                         if (slotInfo.SlotFlags.TokenPresent)
                         {
                             // Получить информацию о токене
-                            TokenInfo tokenInfo = slot.GetTokenInfo();
+                            var tokenInfo = slot.GetTokenInfo();
 
                             // Распечатать информацию о токене
-                            InfoLabel.Text += ("Printing token info:");
-                            InfoLabel.Text += (" Label: {0}", tokenInfo.Label);
-                            InfoLabel.Text += (" Manufacturer: {0}", tokenInfo.ManufacturerId);
-                            InfoLabel.Text += (" Token model: {0}", tokenInfo.Model);
-                            InfoLabel.Text += (" Token #: {0}", tokenInfo.SerialNumber);
-                            InfoLabel.Text += (" Flags: 0x{0:X}", tokenInfo.TokenFlags.Flags);
-                            InfoLabel.Text += (" Max session count: {0}", tokenInfo.MaxSessionCount);
-                            InfoLabel.Text += (" Current session count: {0}", tokenInfo.SessionCount);
-                            InfoLabel.Text += (" Max RW session count: {0}", tokenInfo.MaxRwSessionCount);
-                            InfoLabel.Text += (" Current RW session count: {0}", tokenInfo.RwSessionCount);
-                            InfoLabel.Text += (" Max PIN length: {0}", tokenInfo.MaxPinLen);
-                            InfoLabel.Text += (" Min PIN length: {0}", tokenInfo.MinPinLen);
-                            InfoLabel.Text += (" Total public memory: {0}", tokenInfo.TotalPublicMemory);
-                            InfoLabel.Text += (" Free public memory: {0}", tokenInfo.FreePublicMemory);
-                            InfoLabel.Text += (" Total private memory: {0}", tokenInfo.TotalPrivateMemory);
-                            InfoLabel.Text += (" Free private memory: {0}", tokenInfo.FreePrivateMemory);
-                            InfoLabel.Text += (" Hardware version: {0}", tokenInfo.HardwareVersion);
-                            InfoLabel.Text += (" Firmware version: {0}", tokenInfo.FirmwareVersion);
-                            InfoLabel.Text += (" Timer #: {0}", tokenInfo.UtcTime);
-                            InfoLabel.Text += "\n";
+                            Features.Add(new Feature("Token Label", tokenInfo.Label));
+                            Features.Add(new Feature("Token manufacturer", tokenInfo.ManufacturerId));
+                            Features.Add(new Feature("Token model", tokenInfo.Model));
+                            Features.Add(new Feature("Token #", tokenInfo.SerialNumber));
+                            Features.Add(new Feature("Token hardware version", tokenInfo.HardwareVersion));
+                            Features.Add(new Feature("Token firmware version", tokenInfo.FirmwareVersion));
 
                             // Получить список поддерживаемых токеном механизмов
-                            InfoLabel.Text += ("Checking mechanisms available");
-                            List<CKM> mechanisms = slot.GetMechanismList();
+                            var mechanisms = slot.GetMechanismList();
                             if (mechanisms.Count == 0)
                             {
                                 throw new InvalidOperationException("No mechanism available");
@@ -97,20 +101,8 @@ namespace Xamarin.Info
 
                             foreach (var mechanism in mechanisms)
                             {
-                                MechanismInfo mechanismInfo = slot.GetMechanismInfo(mechanism);
-                                InfoLabel.Text += ("Printing mechanism info:");
-                                if (Enum.IsDefined(typeof(Extended_CKM), (Extended_CKM)mechanismInfo.Mechanism))
-                                {
-                                    InfoLabel.Text += (" Mechanism type: {0}", (Extended_CKM)mechanismInfo.Mechanism);
-                                }
-                                else
-                                {
-                                    InfoLabel.Text += (" Mechanism type: {0}", mechanismInfo.Mechanism);
-                                }
-                                InfoLabel.Text += (" Min key size: {0}", mechanismInfo.MinKeySize);
-                                InfoLabel.Text += (" Max key size: {0}", mechanismInfo.MaxKeySize);
-                                InfoLabel.Text += (" Mechanism flags: 0x{0:X}", mechanismInfo.MechanismFlags.Flags);
-                                InfoLabel.Text += "\n";
+                                var mechanismInfo = slot.GetMechanismInfo(mechanism);
+                                Features.Add(new Feature("Mechanism type", mechanismInfo.Mechanism.ToString()));
                             }
                         }
                     }
@@ -118,11 +110,11 @@ namespace Xamarin.Info
             }
             catch (Pkcs11Exception ex)
             {
-                InfoLabel.Text += ($"Operation failed [Method: {ex.Method}, RV: {ex.RV}]");
+                DisplayAlert("Error", $"Operation failed [Method: {ex.Method}, RV: {ex.RV}]", "OK");
             }
             catch (Exception ex)
             {
-                InfoLabel.Text += ($"Operation failed [Message: {ex.Message}]");
+                DisplayAlert("Error", $"Operation failed [Message: {ex.Message}]", "OK");
             }
         }
     }
